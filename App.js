46109096b906a8ef2100ceb811fb3de6fd6fd77d
Alireza <103531312+alirezashon@users.noreply.github.com>
2022-10-23 06:45:09 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import DrawerNav from './navigations/drawer';
 import CamScanner from './componentes/camScanner';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { color } from 'react-native-reanimated';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,20 +14,16 @@ const Stack = createNativeStackNavigator();
 export default function App () {
   return (
     <NavigationContainer>
+      <StatusBar/>
       <Stack.Navigator  >
   <Stack.Screen 
    options={{ headerShown: false }}
    name='home' component={DrawerNav}/>
-  <Stack.Screen name='اسکنر' component={CamScanner} />
+  <Stack.Screen name='اسکنر' component={CamScanner}  options={{ headerStyle: { 
+         backgroundColor: '#d0ec2f',},        headerTintColor: '#f56918'
+}}/>
 </Stack.Navigator>
     </NavigationContainer>
 
   );
 }
-
-
-
-
-
-
-
