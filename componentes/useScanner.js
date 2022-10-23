c0983e2c7ae1ac583c7ecb37b02b161fe0f1b8e8
Alireza <103531312+alirezashon@users.noreply.github.com>
2022-10-23 06:35:29 -0700

    Update useScanner.js

--- componentes/useScanner.js
+++ componentes/useScanner.js
@@ -1,4 +1,4 @@
-import { View , TouchableOpacity} from "react-native";
+import { View , StyleSheet ,TouchableOpacity} from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
@@ -13,15 +13,22 @@ import { useNavigation } from '@react-navigation/native';
 style={{flexDirection:'row' , 
 justifyContent:'flex-end',
 alignItems:"center",
+justifyContent:'space-around',
+backgroundColor:'#fff',
 padding:12,
 paddingVertical:13
 }}> 
-<Ionicons name="md-chatbubble-ellipses-outline" size={24} color="#0e7a4d" />    
+<Ionicons name="md-chatbubble-ellipses-outline" size={24} color="#fff" style={styles.icons} />    
  <TouchableOpacity
          onPress={()=> navigation.navigate('اسکنر')}>
-                   <AntDesign name="scan1" size={24} color="#0e7a4d" style={{marginLeft:13}}  /> 
+                   <AntDesign name="scan1" size={24} color="#fff" style={styles.icons}  /> 
         </TouchableOpacity>
         </View>
    );
 }
+const styles = StyleSheet.create ({
+icons:{marginLeft:13 , borderWidth:2,paddingHorizontal:7 , paddingVertical:7 ,  borderRadius:15 ,
+backgroundColor:'#a5cd39' ,    borderColor:'#499b01' , textAlign:'center' , justifyContent:'center'
+},
 
+});
